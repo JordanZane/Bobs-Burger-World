c5b04146096acce9c6699f16efe92d4d2ab40463
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-18 10:17:55 +0200

    Add EpisodesList components + Episodes Page, display data by choosen season

---
+++ src/components/EpisodesList/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EpisodesList = ({ episode }) => {
+  return <li>name : {episode.name}</li>;
+};
+
+export default EpisodesList;
--- src/components/Pages/Episodes/index.jsx
+++ src/components/Pages/Episodes/index.jsx
@@ -1,10 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loader from '../../Loader';
+import styled from 'styled-components';
+import EpisodesList from '../../EpisodesList';
+
+const LoaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+const EpisodesMain = styled.div``;
+
+const EpisodesContainer = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const BtnsSortContainer = styled.nav``;
 
 const Episodes = () => {
+  const [data, setData] = useState([]);
+  const [isDataLoading, setDataLoading] = useState(false);
+  const [seasonFilter, setSeasonFilter] = useState(1);
+
+  useEffect(() => {
+    setDataLoading(true);
+    axios
+      .get('https://bobsburgers-api.herokuapp.com/episodes')
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+        setDataLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setDataLoading(false);
+      });
+  }, []);
+
+  function handleSeasonFilter(season) {
+    setSeasonFilter(season);
+  }
+
+  const filteredData = seasonFilter
+    ? data.filter((episode) => episode.season === parseInt(seasonFilter))
+    : data;
+
   return (
-    <div>
-      <h1>Episodes page</h1>
-    </div>
+    <main className="container episodes-page">
+      {isDataLoading ? (
+        <LoaderContainer>
+          <Loader />
+        </LoaderContainer>
+      ) : (
+        <EpisodesMain>
+          <h2>Episodes</h2>
+          <BtnsSortContainer>
+            <select
+              onChange={(e) => handleSeasonFilter(e.target.value)}
+              className="deroulant"
+            >
+              <option value="1">Season 1</option>
+              <option value="2">Season 2</option>
+              <option value="3">Season 3</option>
+            </select>
+          </BtnsSortContainer>
+          <EpisodesContainer className="episodes-container">
+            {filteredData.map((episode, index) => (
+              <EpisodesList key={index} episode={episode} />
+            ))}
+          </EpisodesContainer>
+        </EpisodesMain>
+      )}
+    </main>
   );
 };
 
