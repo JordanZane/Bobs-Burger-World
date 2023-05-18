0449435b3f59facb10061db3753d1e5aedeaf057
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-18 10:25:40 +0200

    Display more informations about informations

--- src/components/EpisodesList/index.jsx
+++ src/components/EpisodesList/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const EpisodesList = ({ episode }) => {
-  return <li>name : {episode.name}</li>;
+  return (
+    <li>
+      <h3>Name : {episode.name}</h3>
+      <p>Episode : {episode.id}</p>
+      <p>Date : {episode.airDate}</p>
+      <p>Total views : {episode.totalViewers}</p>
+    </li>
+  );
 };
 
 export default EpisodesList;
